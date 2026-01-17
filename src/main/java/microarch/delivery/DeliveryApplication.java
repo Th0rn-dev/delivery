7f8774edc1d53cc80757c8360131e1e87e07ca05
th0rn <solnek33@gmail.com>
2026-01-17 14:24:39 +0300

    setted layer architecture

--- src/main/java/microarch/delivery/DeliveryApplication.java
+++ src/main/java/microarch/delivery/DeliveryApplication.java
@@ -1,4 +1,4 @@
-package com.example.delivery;
+package microarch.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/microarch/delivery/DeliveryApplicationTests.java
+++ src/test/java/microarch/delivery/DeliveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.delivery;
+package microarch.delivery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
